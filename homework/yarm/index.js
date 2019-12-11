f95d5eda9a5df2e27b8833fdbb8b37c02e2de2d1
Yaroslav <4cast@ukr.net>
2019-12-11 13:48:52 +0200

    homework task 1+2

--- homework/yarm/index.js
+++ homework/yarm/index.js
@@ -1,1 +1,48 @@
-console.log(`Hello world, from Yaroslav`);
+<script>
+// Palindrom
+let str = '98789';
+str = str.toLowerCase()
+let arr = str.split('')
+let textShift = ''
+let textPop = ''
+let res = ''
+let i
+
+for (i = 0; i < arr.length; i++) {
+    textShift += arr.shift()
+    textPop += arr.pop()
+}
+
+function palidrom() {
+    if (textShift === textPop) {
+        res = 'this word is palidrom'
+    }
+    else {
+        res = 'this word is NOT palidrom'
+    }
+    
+}
+
+palidrom()
+
+console.log(res)
+
+//FizzBuzz
+
+ for (let i = 1; i <= 100; i++) {
+     if (i % 3 === 0 && i % 5 === 0) {
+         console.log('FizzBuzz')
+     }
+     else if (i % 3 === 0) {
+        console.log('Fizz')
+     }
+     else if (i % 5 === 0) {
+        console.log('Buzz')
+     }
+     else {
+         console.log(i)
+     }
+ }
+
+
+</script>
