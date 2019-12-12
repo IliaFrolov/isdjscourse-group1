f75afeda2e994ca41c0e71c81cdaecc3611b3173
yagnyukova <57173101+yagnyukova@users.noreply.github.com>
2019-12-13 01:43:30 +0200

    Update index.js

--- homework/iagn/index.js
+++ homework/iagn/index.js
@@ -8,19 +8,21 @@ let dev3, dev5, dev15;
 function fizzBuzz () {
    let unit = '';
    let count = 0;
-      do {
-         count = count + 1;
-         dev3 = Number.isInteger(count / 3);
-         dev5 = Number.isInteger(count / 5);
-         dev15 = Number.isInteger(count / 15);
-         if (dev15) {
-            unit = 'FizzBuzz';
-         } else if (dev3) {
-            unit = 'Fizz';
-         } else if (dev5){
-            unit = 'Buzz';
-         } else unit = count;
-      console.log(unit);	
+   do {
+      count = count + 1;
+      dev3 = Number.isInteger(count / 3);
+      dev5 = Number.isInteger(count / 5);
+      dev15 = Number.isInteger(count / 15);
+      if (dev15) {
+         unit = 'FizzBuzz';
+      } else if (dev3) {
+         unit = 'Fizz';
+      } else if (dev5){
+         unit = 'Buzz';
+      } else {
+		  unit = count;
+      }
+      console.log(unit);
       } while (count < 100);
 }
 fizzBuzz();
