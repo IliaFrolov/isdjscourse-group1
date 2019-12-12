034a8c8669e930a50fd1e5486804770e2cfcbd51
yagnyukova <57173101+yagnyukova@users.noreply.github.com>
2019-12-13 01:15:39 +0200

    Update index.js

--- homework/iagn/index.js
+++ homework/iagn/index.js
@@ -1,55 +1,60 @@
-//Homework/2/iagn
-//Problem 1
-//Program must return numbers from 1 to 100 including condition that 
-//"Fizz" is returned instead of the number devidable by 3,
-//"Buzz" is returned instead of the number devidable by 5,
-//and "FizzBuzz" for both.
+// Homework/2/iagn
+// Problem 1
+// Program must return numbers from 1 to 100 including condition that 
+// "Fizz" is returned instead of the number devidable by 3,
+// "Buzz" is returned instead of the number devidable by 5,
+// and "FizzBuzz" for both.
 let dev3, dev5, dev15;
 function fizzBuzz () {
 	let unit = '';
 	let count = 0;
 		do {
 			count = count + 1;
-			dev3=Number.isInteger(count/3);
-			dev5=Number.isInteger(count/5);
-			dev15=Number.isInteger(count/15);
+			dev3 = Number.isInteger(count / 3);
+			dev5 = Number.isInteger(count / 5);
+			dev15 = Number.isInteger(count / 15);
 			if (dev15) {
-				unit='FizzBuzz';
+				unit = 'FizzBuzz';
 			} else if (dev3) {
-				unit='Fizz';
+				unit = 'Fizz';
 			} else if (dev5){
-				unit='Buzz';
+				unit = 'Buzz';
 			} else unit = count;
 		console.log(unit);
+			
 		} while (count < 100);
 }
+
 fizzBuzz();
 
-//Problem 2
-//Program must check word, string or number 
-//if its elements' values are centrally simetrical
-function isLucky (initial_str) {
-	let bulResult=false, resultExp = '';
-	let strArray = String(initial_str).split('');//modify initial_str to string data type and fill in the array where elements are symbols of initial_str
-	console.log("Your array has the next following elements: " + strArray);//array output
-	let strLength = strArray.length;//strLength = number of elements in strArray
-	console.log("The length of your array or number of its symbols is: " + strLength);//output
-	//let loopSelector=Number.isInteger(strLength/2);//even=true; odd=false
-	let i=0;//When we compare two different elements out of the array we'll need some indicators to do so
-	let j=strLength-1;
-	do {
-		if (strArray[i]===strArray[j]) {
-			bulResult=true; //we should continue checking other pairs of symbols
-			i=i+1;
-			j=j-1;
-		} else {
-			bulResult=false; //here we can stop since there's no sense to continue, we already know that's not gonna happen
-			i=j+1;
-		}
-	} while (i<j);
-	if (bulResult===true) {
-		resultExp='Yes, you are lucky! Your expression is centrally symmetrical!';
-	} else resultExp='No, not at this time...';
+// Problem 2
+// Program must check word, string or number 
+// if its elements' values are centrally simetrical
+   let bulResult = false, resultExp = '';
+   let strArray = String(initial_str).split('');//modify initial_str to string data type and fill in the array where elements are symbols of initial_str
+   console.log("Your array has the next following elements: " + strArray);//array output
+   let strLength = strArray.length;//strLength = number of elements in strArray
+   console.log("The length of your array or number of its symbols is: " + strLength);//output
+   // let loopSelector=Number.isInteger(strLength/2);//even=true; odd=false
+   let i = 0;//When we compare two different elements out of the array we'll need some indicators to do so
+   let j = strLength - 1;
+   do {
+      if (strArray[i] === strArray[j]) {
+         bulResult=true; //we should continue checking other pairs of symbols
+         i = i + 1;
+         j = j - 1;
+      } else 
+         bulResult=false; //here we can stop since there's no sense to continue, we already know that's not gonna happen
+         i = j + 1;
+   } while (i < j);
+   if (bulResult === true) {
+      resultExp = 'Yes, you are lucky! Your expression is centrally symmetrical!';
+   } else resultExp = 'No, not at this time...';
 	console.log(resultExp);
 }
+
 isLucky(34543);
+isLucky('ertre');
+isLucky('abba');
+isLucky('tyuio');
+isLucky('trewqp');
