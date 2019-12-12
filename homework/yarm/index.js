28e6ef31da645aa8af977d2b4813a6e13ef097b8
Yaroslav <4cast@ukr.net>
2019-12-12 08:29:52 +0200

    remove script tag, change in palindrome condition statements -now return true/false

--- homework/yarm/index.js
+++ homework/yarm/index.js
@@ -1,47 +1,35 @@
-<script>
 // Palindrome
 
-function palindrome(str) {
+function palindrome (str) {
+   str = str.toLowerCase();
+   const arr = str.split('');
+   let textShift = '';
+   let textPop = '';
 
-    str = str.toLowerCase()
-    let arr = str.split('')
-    let textShift = ''
-    let textPop = ''
-    let res = ''
+   for (let i = 0; i < arr.length; i++) {
+      textShift += arr.shift();
+      textPop += arr.pop();
+   }
 
-    for (let i = 0; i < arr.length; i++) {
-        textShift += arr.shift()
-        textPop += arr.pop()
-    }
-
-    if (textShift === textPop) {
-        res = 'this word is palindrome'
-    }
-    else {
-        res = 'this word is NOT palindrome'
-    }
-    
+   if (textShift === textPop) {
+      return true;
+   } else {
+      return false;
+   }
 }
 
-palindrome('Annaanna')
+palindrome('96588');
 
+// FizzBuzz
 
-//FizzBuzz
-
- for (let i = 1; i <= 100; i++) {
-     if (i % 3 === 0 && i % 5 === 0) {
-         console.log('FizzBuzz')
-     }
-     else if (i % 3 === 0) {
-        console.log('Fizz')
-     }
-     else if (i % 5 === 0) {
-        console.log('Buzz')
-     }
-     else {
-         console.log(i)
-     }
- }
-
-
-</script>
+for (let i = 1; i <= 100; i++) {
+   if (i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz');
+   } else if (i % 3 === 0) {
+      console.log('Fizz');
+   } else if (i % 5 === 0) {
+      console.log('Buzz');
+   } else {
+      console.log(i);
+   }
+}
