45d441fea2330220a3ab3ebfa8218c632d1f3f23
Yolka85 <4yolka@gmail.com>
2019-12-16 22:55:21 +0200

    Added exception for value types

--- homework/oshl/index.js
+++ homework/oshl/index.js
@@ -17,18 +17,22 @@
    // Task 2. Palindrome
    console.log('----- Task 2 -----');
    function palindromeChecker (val) {
-      if (!val) {
-         console.log('Please, enter valid value');
+      const _val = typeof val;
+      if (_val !== 'number' && _val !== 'string') {
+         console.log('The value isn\'t a string or a number');
+         return;
+      } else if (!val) {
+         console.log('Please, enter a valid value');
          return;
       }
       const arr = val.toString().toLowerCase().split('');
       for (let n = 1; n <= Math.floor(arr.length / 2); n++) {
          if (arr[n - 1] !== arr[arr.length - n]) {
-            console.log(`${val} is not palindrome`);
+            console.log(`${val} is not a palindrome`);
             return;
          }
       }
-      console.log(`${val} is palindrome`);
+      console.log(`${val} is a palindrome`);
    }
 
    palindromeChecker();
